9108c3e1380d2b5387ebc26640b7fb7553723a28
ThanhTung <qingsongptt@gmail.com>
2019-08-13 12:36:18 +0700

    Homepage

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.static(__dirname + '/views'));
 var server = http.createServer(app);
 var request = require("request");
 
-app.set('port', process.env.HEROKU_NODEJS_PORT || process.env.PORT || 89);
+app.set('port', process.env.HEROKU_NODEJS_PORT || process.env.PORT || 8080);
 app.set('ip', process.env.HEROKU_NODEJS_IP || process.env.IP || "0.0.0.0");
 
 server.listen(app.get('port'), app.get('ip'), function() {
